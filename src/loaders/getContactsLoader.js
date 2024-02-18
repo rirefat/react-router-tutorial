3c892babcf5367c0bbdfa63376fabd6ace761bf2
rirefat <rirefat.official@gmail.com>
2024-02-18 07:18:29 +0600

    done upto form actions

--- src/loaders/getContactsLoader.js
+++ src/loaders/getContactsLoader.js
@@ -1,7 +1,9 @@
+import localforage from "localforage";
 import { getContacts } from "../contacts";
 
-
 export async function getContactsLoader() {
+  // For clearing previous data
+  // localforage.clear();
   const contacts = await getContacts();
   return { contacts };
 }
--- src/main.jsx
+++ src/main.jsx
@@ -5,12 +5,16 @@ import { RouterProvider, createBrowserRouter } from 'react-router-dom'
 import Root from './Root'
 import ErrorPage from './ErrorPage'
 import Contact from './Contact'
+import { getContactsLoader } from './loaders/getContactsLoader'
+import { createContactAction } from './actions/contactsAction'
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Root />,
     errorElement: <ErrorPage />,
+    loader: getContactsLoader,
+    action: createContactAction,
     children: [
       {
         path: "/contacts/:contactId",
